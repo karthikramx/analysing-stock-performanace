dd3e8d869f925b19c33f7f99a9d16af6f01cffea
Karthik Ram <karthikram570@gmail.com>
2021-05-19 14:58:44 +0530

    added file to display tesla historical share and revenue data

--- historical_price_and_revenue.py
+++ historical_price_and_revenue.py
@@ -26,54 +26,31 @@ def make_graph(stock_data, revenue_data, stock):
 
 
 def get_revenue_data(soup):
+    revenue_table = soup.find_all("table", {"class": "historical_data_table table"})
     revenue_data = pd.DataFrame(columns=["Date", "Revenue"])
-    for row in soup.find("tbody").find_all("tr"):
+    for row in revenue_table[1].find("tbody").find_all("tr"):
         col = row.find_all("td")
         date = col[0].text
-        Open = col[1].text
-        high = col[2].text
-        low = col[3].text
-        close = col[4].text
-        adj_close = col[5].text
-        volume = col[6].text
-
-        revenue_data = revenue_data.append(
-            {"Date": date, "Open": Open, "High": high, "Low": low, "Close": close, "Adj Close": adj_close,
-             "Volume": volume}, ignore_index=True)
-
-    print(revenue_data.head())
-    revenue_data.set_index("Date", inplace=True)
+        revenue = col[1].text
+        if revenue:
+            revenue = revenue.replace('$', '').replace(',', '')
+            revenue_data = revenue_data.append({"Date": date, "Revenue": revenue}, ignore_index=True)
     return revenue_data
 
 
 # tesla historical data
 tesla_ticker = yf.Ticker("TSLA")
 tesla_hist_data = tesla_ticker.history(period="max")
+tesla_hist_data.reset_index(inplace=True)
+
 
 # tesla revenue data
 tesla_hist_url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue?cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ&cm_mmc=Email_Newsletter-_-Developer_Ed%2BTech-_-WW_WW-_-SkillsNetwork-Courses-IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork-23455606&cm_mmca1=000026UJ&cm_mmca2=10006555&cm_mmca3=M12345678&cvosrc=email.Newsletter.M12345678&cvo_campaign=000026UJ"
 tesla_page = requests.get(tesla_hist_url)
 tesla_soup = BeautifulSoup(tesla_page.content, "html5lib")
-revenue_table = tesla_soup.find_all("table", {"class": "historical_data_table table"})
-tesla_revenue = pd.DataFrame(columns=["Date", "Revenue"])
-for row in revenue_table[1].find("tbody").find_all("tr"):
-    col = row.find_all("td")
-    date = col[0].text
-    revenue = col[1].text
-    if revenue:
-        revenue = revenue.replace('$', '').replace(',', '')
-        tesla_revenue = tesla_revenue.append({"Date": date, "Revenue": revenue}, ignore_index=True)
+tesla_revenue = get_revenue_data(tesla_soup)
 
-print("debug")
-
-# gamestop historical data
-gs_ticker = yf.Ticker("GME")
-gs_hist_data = gs_ticker.history(period="max")
-
-# game stop revenue data
-gs_hist_url = "https://finance.yahoo.com/quote/GME/history?p=GME"
-gs_page = requests.get(gs_hist_url)
-gs_soup = BeautifulSoup(gs_page.content, "html5lib")
-gs_revenue_data = get_revenue_data(gs_soup)
+# plotting historical share price and revenue
+make_graph(tesla_hist_data, tesla_revenue, "Tesla")
 
 print("debug")
